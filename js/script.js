37cd68fb258df85b02a2872f0e6085ae47132621
Aditya Singh Rawat <adirajan29@gmail.com>
2026-02-03 00:03:10 +0530

    style: improve task list UI and visual hierarchy

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function openFeatures(){
-    var allElems=document.querySelectorAll(".elem")
+var allElems=document.querySelectorAll(".elem")
 var fullElemPage=document.querySelectorAll('.fullElem')
 var fullElemPageBackBtn=document.querySelectorAll('.fullElem .back')
 
@@ -16,3 +16,15 @@ fullElemPageBackBtn.forEach(function(back){
 })
 }
 openFeatures()
+
+let form = document.querySelectorAll('.addtask form')  
+let taskInput=document.querySelectorAll(".addTask from task-input")
+let taskDetailsInput=document.querySelectorAll('.addTask from Textarea')
+let taskCheckbox= document.querySelectorAll('.addTask form #check')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()   // as forms reload the page so to prevent this action we write prevent default function of the form 
+    console.log(taskInput.value);
+    console.log(taskDetailsInput.value); 
+    console.log(taskCheckbox.checked);
+})
